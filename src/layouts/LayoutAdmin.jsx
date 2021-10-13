ca1d6b2f439337f0305dc4900fa8c7958d8f6668
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-10-12 19:05:28 -0500

    se reinstalaron las extensiones de prediccion de tailwind

--- src/layouts/LayoutAdmin.jsx
+++ src/layouts/LayoutAdmin.jsx
@@ -5,8 +5,7 @@ import Sidebar from '../components/sidebar'
 const LayoutAdmin = ({children}) => {
     return (
         <div className='flex w-screen h-screen'>
-            <divgit add .
-            >
+            <div className ='flex flex-nowrap w-full h-full'>
                 <Sidebar/>
                 <main className='flex w-screen bg-green-50'>
                     {children}
