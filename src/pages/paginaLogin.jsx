a463c6c2270493b73a866c5850f29b8a8bbe28a0
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-05 15:15:36 -0500

    se modificó el botón de google y se le dio la funcionalidad para que redirija a el login de auth0

--- src/pages/paginaLogin.jsx
+++ src/pages/paginaLogin.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 import logo from '../media/logo3.png'
 import fondo from '../media/fondo.jpeg'
 import LayoutLogin from '../layouts/LayoutLogin'
-import Google from '../media/google_logo.png';
+import { useAuth0 } from "@auth0/auth0-react";
 
-const paginaLogin = () => {
+const PaginaLogin = () => {
+    const { loginWithRedirect } = useAuth0();
     return (
         <LayoutLogin> 
             
-                <img src= {fondo} alt="imagen fondo" width = '1922px' height = '915px'/>             
+            <div className='flex w-full h-1/4'>
+                <img src= {fondo} alt="imagen fondo"/>             
+            </div>
             
 
             <div className = 'rectanguloLogin flex flexcol items-center justify-center' >
                     <img src= {logo} alt="imagen fondo" className='w-96 h-96'/>
                 
-                    <button type='submit'
+                    <button onClick={() => loginWithRedirect()} type='submit'
                             className='h-10 py-2 px-4 m-8 border border-transparent text-sm font-medium rounded-md text-gray-700 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500'
                         >
                             <div className='flex items-center justify-start'>
-                            <img src={Google} alt='Logo Google' className='h-6 w-6' />
-                            <span className='mx-4'>Continúa con Google</span>
+                            <span className='mx-4'>LOGIN</span>
                             </div>
                     </button>
             </div>
@@ -29,7 +31,7 @@ const paginaLogin = () => {
     )
 }
 
-export default paginaLogin
+export default PaginaLogin
 
 
 
