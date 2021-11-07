54b40c22e904669bad672f0fae5c335512c206d7
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-07 15:45:23 -0500

    estilos contenedor página home

--- src/pages/admin/home.jsx
+++ src/pages/admin/home.jsx
@@ -8,7 +8,7 @@ const home = () => {
                     Home
                 </BarraTitulo>
 
-                <div className='w-60 h-60 bg-blue-800'>
+                <div className='w-60 h-60 bg-blue-300 text-white rounded-md'>
                     Bienvenido...Esta es la página de gestión de ventas
                     navega por cada uno de los botones de la barra lateral izquierda para acceder a cada uno de los módulos.
                 </div>
