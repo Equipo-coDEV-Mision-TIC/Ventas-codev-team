cc26822b901b56791ace7017e51063a4a30b3b31
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-07 18:55:07 -0500

    prueba3 url

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import {
 
 function App() {
   return (
-    <Auth0Provider domain="ventas-misiontic.us.auth0.com" clientId="3rfEJuOcWXxkmNFsQJXjZ06p2VCNjYPl" redirectUri='https://polar-sea-28372.herokuapp.com/Home'>
+    <Auth0Provider domain="ventas-misiontic.us.auth0.com" clientId="3rfEJuOcWXxkmNFsQJXjZ06p2VCNjYPl" redirectUri='http://polar-sea-28372.herokuapp.com/Home'>
       <div>
         <Router>
           <Switch>
--- src/pages/paginaLogin.jsx
+++ src/pages/paginaLogin.jsx
@@ -3,7 +3,7 @@ import logo from '../media/logo3.png'
 import fondo from '../media/fondo.jpeg'
 import LayoutLogin from '../layouts/LayoutLogin'
 import { useAuth0 } from "@auth0/auth0-react";
-import {Link} from 'react-router-dom'
+
 
 const PaginaLogin = () => {
     const { loginWithRedirect } = useAuth0();
@@ -18,7 +18,7 @@ const PaginaLogin = () => {
             <div className = 'rectanguloLogin flex flexcol items-center justify-center' >
                     <img src= {logo} alt="imagen fondo" className='w-96 h-96'/>
             
-                        <button onClick={() => loginWithRedirect()} type='submit'
+                        <button onClick={() => loginWithRedirect()}
                                 className='h-10 py-2 px-4 m-8 border border-transparent text-sm font-medium rounded-md text-gray-700 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500'
                             >
                                 <div className='flex items-center justify-start'>
