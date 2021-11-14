646198ef92a0474c430368bfb8f3a33fae30b9fb
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-14 12:53:22 -0500

    cambiar url para despliegue v3

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-// const baseURL = "http://localhost:5000"
+//const baseURL = "http://localhost:5000"
 const baseURL = 'https://thawing-springs-56523.herokuapp.com';
 
 //CRUD PARA PRODUCTOS
